cca3d1ba7852121fdb4a8a3a1c584258809a4369
World_Languages <mundofinky@gmail.com>
2018-09-18 20:21:16 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// WIP
+
+var div = document.createElement("span");
+div.innerHTML = '<div class="button grey small" style="margin-left:10px;color:red;font-weight:bold;" id="WLpromote"><span>Promote curator</span></div><div class="button grey small" style="margin-left:10px;color:red;font-weight:bold;" id="WLremove"><span>Remove curator</span></div><div class="button grey small" style="margin-left:10px;color:red;font-weight:bold;" id="WLleave"><span>Leave studio</span></div>';
+document.getElementById("curator-action-bar").appendChild(div);
